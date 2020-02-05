d6de1a1298bf0229fb9217869bd89cb90258081d
Belinda Caylor <belinda.caylor@gmail.com>
2020-02-05 10:19:17 -0600

    feat: createDestDropDown

--- src/client/js/defineTrip.js
+++ src/client/js/defineTrip.js
@@ -3,15 +3,54 @@ let tripCount = 1;
 const body = document.getElementById('body')
 let tripArray = [];
 const tripID = "trip" + tripCount;
+const testGeoNamesData = require('./sampleGeoNamesData.js');
 // require("regenerator-runtime");
 
-function createDestDropDown(geoNames) {
-  // console.log(geoNames);
+function createDestDropDown(geoNames, formDestination) {
+  let placeNames = [];
+  // console.log(placeNames.length);
+  const postalcode = geoNames.postalCodes;
+  console.log(postalcode);
 
+  postalcode.forEach((postalcode) => {
+    if (formDestination === postalcode.placeName) {
+      let newEntry = {
+        placeName: postalcode.placeName,
+        adminName1: postalcode.adminName1,
+        countryCode: postalcode.countryCode,
+        lng: postalcode.lng,
+        lat: postalcode.lat
+      };
+
+      if (placeNames.length === 0) {
+        placeNames.push(newEntry);
+      }else{
+        for (const place of placeNames) {
+          if (postalcode.adminName1 === place.adminName1) {
+            continue;
+          }else{
+            placeNames.push(newEntry);
+            console.log(placeNames);
+            break;
+          };
+        };
+      };
+    };
+
+  });
+  console.log(placeNames);
 };
 
 function hndlDestinationSubmit (event) {
   let formDestination = document.getElementById('destination').value;
+  // const geoNames = getGeoNames(formDestination);
+    // .then(() => {
+    //   console.log(geoNames);
+    //   // createDestDropDown(geoNames);
+    //   }
+    // )
+  createDestDropDown(testGeoNamesData, formDestination);
+
   // let formStart = document.getElementById('start').value;
   // let formEnd = document.getElementById('end').value;
   // console.log(formDestination + ' ' + formStart + ' ' + formEnd);
@@ -19,12 +58,15 @@ function hndlDestinationSubmit (event) {
   //Hide Input form
 
   //Create Location Dropdown
-  const geoNames = getGeoNames(formDestination);
-  .then(() => {
-    console.log(geoNames);
-    // createDestDropDown(geoNames);
-    }
-  )
+
+
+
+
+  // .then(() => {
+  //   console.log(geoNames);
+  //   // createDestDropDown(geoNames);
+  //   }
+  // )
 
 };
 
@@ -56,4 +98,14 @@ const testgetGeoNames = async (Dest) => {
   }
 };
 
+function toggleElement(ID) {
+  var x = document.getElementById(ID);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+// module.exports = hndlDestinationSubmit
 export { hndlDestinationSubmit };
---
+++ src/client/js/sampleGeoNamesData.js
@@ -0,0 +1,9 @@
+let sampleAPI = {"postalCodes":[
+  {"adminCode2":"023","adminCode1":"SD","adminName2":"Charles Mix","lng":-98.281876,"countryCode":"US","postalCode":"57380","adminName1":"South Dakota","ISO3166-2":"SD","placeName":"Wagner","lat":43.081931},
+  {"adminCode2":"502","adminCode3":"50201","adminName3":"Abtenau","adminCode1":"05","adminName2":"Politischer Bezirk Hallein","lng":13.327313332271578,"countryCode":"AT","postalCode":"5441","adminName1":"Salzburg","ISO3166-2":"5","placeName":"Wagner","lat":47.588000621843676},
+  {"adminCode2":"2933406","adminCode1":"05","lng":-41.2209518695652,"countryCode":"BR","postalCode":"46970-000","adminName1":"Bahia","placeName":"Wagner","lat":-12.259299173913},
+  {"adminCode2":"029","adminCode1":"OR","adminName2":"Jackson","lng":-122.78605,"countryCode":"US","postalCode":"97540","adminName1":"Oregon","ISO3166-2":"OR","placeName":"Talent","lat":42.236252},
+  {"adminCode2":"4200705","adminCode1":"26","lng":-49.3406887470356,"countryCode":"BR","postalCode":"88450-000","adminName1":"Santa Catarina ","placeName":"Alfredo Wagner","lat":-27.704811660079}
+]}
+
+module.exports = sampleAPI
