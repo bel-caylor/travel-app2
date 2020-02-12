68dcbfebb16fb2ecc8f3d086d38554d12906afc6
Belinda Caylor <belinda.caylor@gmail.com>
2020-02-12 13:31:41 -0600

    feat: add Service workers and setup production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,45 @@
 const path = require('path');
 const webpack = require('webpack');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'production',
-}
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  output: {
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+  module: {
+  	rules: [
+  		{
+  			test: '/\.js$/',
+  			exclude: /node_modules/,
+  			loader: 'babel-loader'
+  		},
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+  	]
+  },
+  plugins: [
+	new HTMLWebPackPlugin({
+		template: './src/client/html/index.html',
+		filename: 'index.html'
+		}
+	),
+  new MiniCssExtractPlugin(),
+  new WorkboxPlugin.GenerateSW()
+]
+};
